58c19a1b30a6d1d0ed7957f14b16521b9e3498be
woomy-boy <66823345+woomy-boy@users.noreply.github.com>
2020-06-17 17:13:02 +0200

    my 'code'

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   string CharName = "John";
+   string DadComp = "Gamer INC.";
+   int CharAge ;
+   CharAge = 13;
+   int NewCharAge;
+   NewCharAge = 21;
+    cout << "there was a man named " << CharName << endl;
+    cout << "he's dad, Mike is a co-founder of " << DadComp << endl;
+    cout << "and he's mother is dead so we dont talk about her." << endl;
+    cout << "John does his homework." << endl;
+    cout << "here is " << CharName  << endl ;
+    cout << "        /                |" << endl;
+    cout << "       /                 | " << endl;
+    cout << "      /                  |" << endl;
+    cout << "     /                   |" << endl;
+    cout << "     _____________________" << endl;
+    cout << " see, he isnt a cool guy, is he?" << endl;
+    cout <<" he is not." << CharAge << endl;
+    cout << "his ture age is " << NewCharAge << endl;
+
+    return 0;
+}
