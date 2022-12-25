ea2cde960680c4a2a608a4a8b4b678f4669ff54c
woomy-boy <66823345+woomy-boy@users.noreply.github.com>
2022-12-25 09:53:55 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,23 +10,21 @@ int main()
    CharAge = 13;
    int NewCharAge;
    NewCharAge = 21;
-   int hviteCum;
-   hviteCum = bye
     cout << "there was a man named " << CharName << endl;
-    cout << "he's dad, Mike is a co-founder of " << DadComp << endl;
-    cout << "and he's mother is dead so we dont talk about her." << endl;
+    cout << "his dad, Mike is a co-founder of " << DadComp << endl;
+    cout << "and his mother is dead so we dont talk about her." << endl;
     cout << "John does his homework." << endl;
     cout << "here is " << CharName  << endl ;
-    cout << "(Yes this is supposed to be an image of" << CharName "." << endl;
+    cout << "(Yes this is supposed to be an image of " << CharName; cout << ".)" << endl;
     cout << "        /                |" << endl;
     cout << "       /                 | " << endl;
     cout << "      /                  |" << endl;
     cout << "     /                   |" << endl;
     cout << "     _____________________" << endl;
     cout << " see, he isnt a cool guy, is he?" << endl;
-    cout <<" he is not." << CharAge << endl;
-    cout << "his ture age is " << NewCharAge << endl;
-   cout << "here is me trying and than failing to write cpp " << hviteCum << endl;
-   
+    cout <<" he is not " << CharAge; cout << " is he???" << endl;
+    cout << "his ture age is " << NewCharAge; cout << "." << endl;
+   cout << "here is me trying and than failing to write cpp. " << endl;
+
     return 0;
 }
